a4c25eb3aad5662a9368a9be49a0d42b41eb66c8
Francisco Abarca <franciscoabarcaperez@gmail.com>
2021-03-13 10:43:58 -0600

    Controller Categorias

---
+++ ApiTest/Controllers/CategoriasController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiTest.Models;
+namespace ApiTest.Controllers {
+    [Route( "api/[controller]" )]
+    [ApiController]
+    public class CategoriasController : ControllerBase {
+        public _dbContext _context { get; }
+        public CategoriasController(_dbContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Categoria> GetAll() {
+            return (from a in _context.Categorias
+                    select a).ToArray();
+        }
+    }
+
+}
